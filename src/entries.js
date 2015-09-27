38d047667886e7714205011a61909b32fdf1aaca
Roberto Serrano <rserranon@gmail.com>
2015-09-27 18:33:16 -0500

    Initial commit

---
+++ src/entries.js
@@ -0,0 +1,41 @@
+import {inject} from 'aurelia-framework';
+import {HttpClient} from 'aurelia-fetch-client';
+import 'fetch';
+
+@inject(HttpClient)
+export class Entries{
+  heading = 'Entries';
+  entries = [];
+
+  constructor(http){
+    http.configure(config => {
+        config
+        .withBaseUrl('http://rsn.com:8080/')
+        .withDefaults({
+            headers: {
+                'Accept': 'application/json',
+                'X-Requested-With': 'Fetch',
+                'Access-Control-Allow-Origin': 'http://rsn.com'
+            }
+        })
+        .withInterceptor({
+            request(request) {
+                console.log(`Requesting ${request.method} ${request.url}`);
+                return request; // you can return a modified Request, or you can short-circuit the request by returning a Response
+            },
+            response(response) {
+                console.log(`Received ${response.status} ${response.url}`);
+                return response; // you can return a modified Response
+            }
+        });	
+    this.http = http;
+	}
+  )}
+
+  activate(){
+    return this.http.fetch('entry/index.json')
+      .then(response => response.json())
+      .then(entries =>  this.entries = entries);
+  }
+}
+
