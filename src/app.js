7145c4ec804fb896de1714c766244b36ca0a36d2
Roberto Serrano <rserranon@gmail.com>
2015-10-03 15:40:10 -0500

    Added font-awesome icons

--- src/app.js
+++ src/app.js
@@ -2,10 +2,9 @@ export class App {
   configureRouter(config, router){
     config.title = 'Aurelia';
     config.map([
-      { route: ['','welcome'],  name: 'welcome',      moduleId: 'welcome',      nav: true, title:'Welcome' },
-      { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title:'Github Users' },
-      { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title:'Child Router' },
-      { route: 'entries',  		name: 'entries', 	  moduleId: 'entries', 		nav: true, title:'Entries' }
+      { route: ['', 'timeline'], name: 'timeline', moduleId: 'timeline', nav: true, title:'Timeline',  settings: 'fa fa-heartbeat'},
+      { route: 'users',          name: 'users',    moduleId: 'users',    nav: true, title:'Contacts' , settings: 'fa fa-users'},
+      { route: 'entry',          name: 'entry',    moduleId: 'entry',    nav: true, title:'New Entry', settings: 'fa fa-stethoscope'}      
     ]);
 
     this.router = router;
