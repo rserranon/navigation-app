3f5bf5dbce538c1beb250d1d412f45ac04756938
Roberto Serrano <rserranon@gmail.com>
2015-10-03 18:04:02 -0500

    Reorganized directories

--- src/app.js
+++ src/app.js
@@ -2,9 +2,9 @@ export class App {
   configureRouter(config, router){
     config.title = 'Aurelia';
     config.map([
-      { route: ['', 'timeline'], name: 'timeline', moduleId: 'timeline', nav: true, title:'Timeline',  settings: 'fa fa-heartbeat'},
-      { route: 'users',          name: 'users',    moduleId: 'users',    nav: true, title:'Contacts' , settings: 'fa fa-users'},
-      { route: 'entry',          name: 'entry',    moduleId: 'entry',    nav: true, title:'New Entry', settings: 'fa fa-stethoscope'}      
+      { route: ['', 'timeline'], name: 'timeline',  moduleId: './timeline/timeline', nav: true, title:'Timeline',  settings: 'fa fa-heartbeat'},
+      { route: 'users',          name: 'users',     moduleId: './contacts/users',    nav: true, title:'Contacts' , settings: 'fa fa-users'},
+      { route: 'entry',          name: 'entry',     moduleId: './entryCRUD/entry',    nav: true, title:'New Entry', settings: 'fa fa-stethoscope'}      
     ]);
 
     this.router = router;
