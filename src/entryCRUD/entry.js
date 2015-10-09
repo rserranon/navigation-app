dbfc4b174ae5149789a2d5210c171809b85dbaa2
Roberto Serrano <rserranon@gmail.com>
2015-10-09 18:05:57 -0500

    Toggle visits

--- src/entryCRUD/entry.js
+++ src/entryCRUD/entry.js
@@ -1,3 +1,28 @@
 export class Entry{
 	heading = 'Entry';
+
+	isVisit		= false;
+	isDiagnose  = false;
+	isTreatment  = false;
+	isProcedure  = false;
+
+	resetEntries () {
+		this.isVisit		 = false;
+		this.isDiagnose   = false;
+		this.isTreatment  = false;
+		this.isProcedure  = false;
+	}
+
+	activate(){
+		this.resetEntries();
+	}
+
+	toggleDiagnose () {
+		this.resetEntries();
+		this.isDiagnose = !this.isDiagnose;
+	}
+	toggleVisit () {
+		this.resetEntries();
+		this.isVisit = !this.isVisit;
+	}
 }
