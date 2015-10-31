8be131e7f828134e5a2ac928567c0374e5870a70
Roberto Serrano <rserranon@gmail.com>
2015-10-30 23:18:49 -0600

    Fixed     repeat.for="file of selectedFiles | fileListToArray"  with Converters

--- src/entryCRUD/entry.js
+++ src/entryCRUD/entry.js
@@ -1,14 +1,18 @@
 export class Entry{
+	// Related to Entry visuals
 	heading = 'Entry'
-
 	entryType = 'Visit'
 	fontAwesomeIcon = 'fa fa-hospital-o'
-
 	isVisit		 = false
 	isDiagnose   = false
 	isStudy	  	 = false
 	isTreatment  = false
 	isProcedure  = false
+
+//  to store Form Files
+	filesArray = []
+	selectedFiles;
+
 
 	constructor() {
       this.file = ''	
@@ -63,18 +67,37 @@ export class Entry{
 		this.fontAwesomeIcon = 'fa fa-ambulance';	
 	};	
 
+	multipleFileSelected() {
+	    this.imageLoading = true;
+	    this.filesArray = [];
+	    this.filesArray = this.$event.target.files;
+	    
+	    for (var i = 0; i < this.filesArray.length; i++) {
+	    	let reader = new FileReader();
+		    let file = this.filesArray[i];
+		    reader.readAsDataURL(file);
+		    console.log ("File Name: " + file.name);
+		    reader.onload = () => {
+		        this.filesArray[i] = reader.result;
+		        console.log (this.filesArray[i]);
+		        console.log ("filesArray: " + this.filesArray); // console ouput: filesArray: [object FileList]
+		    };
+		}
+		this.imageLoading = false;
+	}    
+
 	fileSelected() {
-    this.imageLoading = true;
-    let reader = new FileReader();
-    let file = this.$event.target.files[0];
-    reader.readAsDataURL(file);
-    this.fileName = file.name;
-    console.log ("File Name: " + this.fileName);
-    reader.onload = () => {
-        this.file = reader.result;
-        console.log ("File Content: " + this.file);
-        this.imageLoading = false;  // we need to find a place later in the flow to deactivate imageLoad
+	    this.imageLoading = true;
+	    let reader = new FileReader();
+	    let file = this.$event.target.files[0];
+	    reader.readAsDataURL(file);
+	    this.fileName = file.name;
+	    console.log ("File Name: " + this.fileName);
+	    reader.onload = () => {
+	        this.file = reader.result;
+	        console.log ("File Content: " + this.file);
+	        this.imageLoading = false;  // we need to find a place later in the flow to deactivate imageLoad
     };
     
-}
+	}
 }
